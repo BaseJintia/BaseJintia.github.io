d3d235a69e3d45a1f4e52fd5743596231a995c54
BaseJintia <137229358+BaseJintia@users.noreply.github.com>
2025-01-14 11:52:07 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,53 +1,179 @@
 const products = [
     {
       id: "1",
-      productName: "Territorio 1",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%201.png",
     },
     {
       id: "2",
-      productName: "Territorio 2",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%202.png",
     },
     {
       id: "3",
-      productName: "Territorio 3",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%203.png",
     },
     {
       id: "4",
-      productName: "Territorio 4",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%204.png",
     },
     {
       id: "5",
-      productName: "Territorio 5",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%205.png",
     },
     {
       id: "6",
-      productName: "Territorio 6",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%206.png",
     },
     {
       id: "7",
-      productName: "Territorio 7",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%207.png",
     },
     {
       id: "8",
-      productName: "Territorio 8",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%208.png",
     },
     {
       id: "9",
-      productName: "Territorio 9",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%209.png",
     },
     {
       id: "10",
-      productName: "Territorio 10",
       img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2010.png",
+    },
+    {
+      id: "11",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2011.png",
+    },
+    {
+      id: "12",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2012.png",
+    },
+    {
+      id: "13",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2013.png",
+    },
+    {
+      id: "14",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2014.png",
+    },
+    {
+      id: "15",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2015.png",
+    },
+    {
+      id: "16",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2016.png",
+    },
+    {
+      id: "17",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2017.png",
+    },
+    {
+      id: "18",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2018.png",
+    },
+    {
+      id: "19",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2019.png",
+    },
+    {
+      id: "20",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2020.png",
+    },
+    {
+      id: "21",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2021.png",
+    },
+    {
+      id: "22",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2022.png",
+    },
+    {
+      id: "23",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2023.png",
+    },
+    {
+      id: "24",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2024.png",
+    },
+    {
+      id: "25",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2025.png",
+    },
+    {
+      id: "26",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2026.png",
+    },
+    {
+      id: "27",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2027.png",
+    },
+    {
+      id: "28",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2028.png",
+    },
+    {
+      id: "29",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2029.png",
+    },
+    {
+      id: "30",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2030.png",
+    },
+    {
+      id: "31",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2031.png",
+    },
+    {
+      id: "32",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2032.png",
+    },
+    {
+      id: "33",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2033.png",
+    },
+    {
+      id: "34",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2034.png",
+    },
+    {
+      id: "35",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2035.png",
+    },
+    {
+      id: "36",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2036.png",
+    },
+    {
+      id: "37",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2037.png",
+    },
+    {
+      id: "38",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2038.png",
+    },
+    {
+      id: "39",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2039.png",
+    },
+    {
+      id: "40",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2040.png",
+    },
+    {
+      id: "41",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2041.png",
+    },
+    {
+      id: "42",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2042.png",
+    },
+    {
+      id: "43",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2043.png",
+    },
+    {
+      id: "44",
+      img: "https://raw.githubusercontent.com/BaseJintia/BaseJintia.github.io/refs/heads/main/Tarjetas/Territorio%2044.png",
     },
   ];
   const shopContent = document.getElementById("shopContent");
